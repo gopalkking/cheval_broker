a38f706bdd42723c3ee137b8642002b0b8b2e7c3
gopalkking <gopal392002@gmail.com>
2025-03-10 15:04:12 +0530

    changes

--- lib/src/Views/Screens/booking_screen.dart
+++ lib/src/Views/Screens/booking_screen.dart
@@ -10,6 +10,6 @@ class BookingScreen extends StatefulWidget {
 class _BookingScreenState extends State<BookingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
