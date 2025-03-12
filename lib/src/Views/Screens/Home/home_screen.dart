156106aac697e3abbb5ad310f8848f7775ffd4ef
gopalkking <gopal392002@gmail.com>
2025-03-12 15:28:50 +0530

    HomeScreen

--- lib/src/Views/Screens/Home/home_screen.dart
+++ lib/src/Views/Screens/Home/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cheval_broker/src/Views/Screens/Home/navigator.dart';
 import 'package:cheval_broker/src/Views/Utilies/images.dart';
 import 'package:cheval_broker/src/Views/Widgets/appbar_widget.dart';
 import 'package:cheval_broker/src/Views/Widgets/sizedbox.dart';
@@ -16,6 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController search = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
       appBar: AppbarWidget(
           title: Image.asset(
@@ -48,25 +48,121 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ],
           centerTitile: true),
-      body: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Column(
-          children: [
-            SizedBox(
-                width: MediaQuery.of(context).size.width / 1,
-                child: Center(
-                    child: Textformfieldwidget(
-                  textEditingController: search,
-                  borderradius: 24,
-                  hinttext: "Search",
-                  hintcolor: Colors.white,
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width / 1,
+                  child: Center(
+                      child: Textformfieldwidget(
+                    textEditingController: search,
+                    borderradius: 24,
+                    hinttext: "Search",
+                    hintcolor: Colors.white,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ))),
+              16.vspace,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      countContainer(
+                          Appimage.totalBookings, "12", "Total Booking"),
+                      8.vspace,
+                      countContainer(Appimage.completedEnquiries, "05",
+                          "Completed Enquiries"),
+                      8.vspace,
+                      countContainer(Appimage.totalDrivers, "05", "Total Drivers")
+                    ],
                   ),
-                )))
-          ],
+                  Column(
+                    children: [
+                      countContainer(
+                          Appimage.newEnquiries, "45", "New Enquiries"),
+                      8.vspace,
+                      countContainer(
+                          Appimage.activeEnquiries, "05", "Active \nEnquiries"),
+                      8.vspace,
+                      countContainer(
+                          Appimage.totalVechicle, "05", "Total Vechicle")
+                    ],
+                  )
+                ],
+              ),
+              16.vspace,
+              Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: Colors.black),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Daily Availability",style: theme.textTheme.bodyLarge,),
+                    TextButton(onPressed: (){}, child: Text("View all",style: theme.textTheme.bodyMedium!.copyWith(decoration: TextDecoration.underline,decorationColor: Colors.white),)),
+
+                  ],
+                ),
+//  ListView.builder(
+//                       shrinkWrap: true,
+//                       physics: NeverScrollableScrollPhysics(),
+//                       itemBuilder: (context,index){return Container(
+//                         padding: const EdgeInsets.all(16),
+//                             decoration: BoxDecoration(
+//                                 borderRadius: BorderRadius.circular(8), color: theme.scaffoldBackgroundColor),
+//                         child: Column(
+//                         children: [
+//                                    Text("#24637",style: theme.textTheme.headlineSmall,)
+//                         ],
+//                                       ),
+//                       );})
+              ],
+            ),
+              )
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget countContainer(
+    String image,
+    String countvalue,
+    String containername,
+  ) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2.3,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8), color: Colors.black),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [                                    
+              Image.asset(image),
+              Text(
+                countvalue,
+                style: Theme.of(context).textTheme.headlineSmall,
+              )
+            ],
+          ),
+          8.vspace,
+          Text(
+            containername,
+            style: Theme.of(context).textTheme.bodyLarge,
+          )
+        ],
       ),
     );
   }
--- lib/src/Views/Utilies/images.dart
+++ lib/src/Views/Utilies/images.dart
@@ -9,4 +9,10 @@ class Appimage {
   static const String booking = 'assets/images/Booking.png';
   static const String vechicleList = 'assets/images/vechicle-list.png';
   static const String driverList = 'assets/images/driver-list.png';
+  static const String totalBookings = 'assets/images/total_booking.png';
+  static const String completedEnquiries = 'assets/images/completed_enqueries.png';
+  static const String totalDrivers = 'assets/images/total_drivers.png';
+  static const String newEnquiries = 'assets/images/new_enquiries.png';
+  static const String activeEnquiries = 'assets/images/active_enquiries.png';
+  static const String totalVechicle = 'assets/images/total_vechicle.png';
 }
